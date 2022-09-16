274dca37867f1f8d184802a9b0ba80af2c3fa073
SanaG1002 <92936779+SanaG1002@users.noreply.github.com>
2022-09-16 12:13:20 -0400

    v40

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 //Update cache names any time any of the cached files change.
-const CACHE_NAME = 'static-cache-v39';
+const CACHE_NAME = 'static-cache-v40';
 
 //Add list of files to cache here.
 const FILES_TO_CACHE = [
