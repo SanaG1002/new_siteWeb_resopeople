00b03222877debf2061e04c9382de7db801350a6
SanaG1002 <92936779+SanaG1002@users.noreply.github.com>
2022-09-16 11:39:38 -0400

    v37 style btn install 5

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 //Update cache names any time any of the cached files change.
-const CACHE_NAME = 'static-cache-v36';
+const CACHE_NAME = 'static-cache-v37';
 
 //Add list of files to cache here.
 const FILES_TO_CACHE = [
